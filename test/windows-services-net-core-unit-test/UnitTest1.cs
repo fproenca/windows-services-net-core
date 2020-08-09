f0c97bbc25c99095a1d6f2ff8f5650061c56264d
Francisco Proença <francisco-proenca@hotmail.com>
2020-08-09 14:49:12 -0300

    add folders src and test and add unit test project

---
+++ test/windows-services-net-core-unit-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace windows_services_net_core_unit_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
